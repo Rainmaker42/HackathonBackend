59866b9715e2acd79d16873fe7152521968decf9
rainmaker42 <crawdadincorporated@gmail.com>
2019-06-14 10:49:09 -0400

    First Commit

---
+++ src/main/java/hackathon/hackthonAPI/HackthonApiApplication.java
@@ -0,0 +1,25 @@
+package hackathon.hackthonAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HackthonApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HackthonApiApplication.class, args);
+
+
+	}
+
+	@GetMapping(value = "/test")
+	public String echoTest()
+	{
+		return "Test";
+	}
+
+}
